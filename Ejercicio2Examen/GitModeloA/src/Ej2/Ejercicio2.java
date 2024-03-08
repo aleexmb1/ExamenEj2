c50002d944c9a89bebc0d884c921ef6380c349ad
aleexmb1 <159763520+aleexmb1@users.noreply.github.com>
2024-03-08 11:11:17 +0100

    "Cambio Repositorio Git Hub

--- Ejercicio2Examen/GitModeloA/src/Ej2/Ejercicio2.java
+++ Ejercicio2Examen/GitModeloA/src/Ej2/Ejercicio2.java
@@ -5,6 +5,7 @@ public class Ejercicio2 {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola a tod@s");
+		System.out.println("Cambio en repositorio Git Hub");
 	}
 
 }
